d6750170b1bba77208b95858daa1c5e718077ea7
Sir Eric <echeungsk@gmail.com>
2020-03-29 04:25:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+// Write your solution in this fil
+var customerName = 'Eric'
+function upperCaseCustomerName(customerName) {
+  return customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+  return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+  return bestCustomer = 'Ada';
+}
+
+const leastFavoriteCustomer = 'bob'
+
+function changeLeastFavoriteCustomer(){
+  return const leastFavoriteCustomer = 'not bob'
+}
